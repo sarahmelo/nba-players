632edac7897f5fc6bd420e5e58f51d432fd91a23
sarahmelo <sarahcbdg@gmail.com>
2024-03-10 03:16:53 -0300

    configuration of sqlite and create tables

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,58 +1,26 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const sqlite3 = require('sqlite3');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = 3001
+var db = new sqlite3.Database('database.sqlite')
+const corsOptions = {
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+};
 const sequelize = new Sequelize ({
     dialect: 'sqlite',
-    storage: './database.sqlite'
+    storage: './database.db'
 })
-var db = {}
 
-async function setupDB() {
+async function setupApp() {
     try {
-        db.Player = sequelize.define('Player', {
-            firstName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            lastName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            id: {
-                type: DataTypes.NUMBER,
-                allowNull: false,
-                primaryKey: true,
-            },
-            conference: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            division: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            city: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            abbreviation: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            
-        });
-        
-
-        // Team.hasMany(Player, { foreignKey: 'player'});
-
         await sequelize.sync({ force: true });
-        await db.Player.create({
-            firstName: "Sarah",
-            lastName: "Melo",
-            id: 1,
-            abbreviation: 'SM',
-            city: "São Gonçalo",
-            division: "C",
-            conference: "Não Sei ORG",
-        })
+        app.use(cors(corsOptions));
+        app.use(express.json())
 
     } catch (error) {
         console.error(error);
@@ -61,47 +29,132 @@ async function setupDB() {
 
 async function startServer() {
     try {
-        await setupDB();
+        await setupApp();
 
-        const port = 3001
-        const express = require('express');
-        const app = express();
+        app.post('/api/players', async (req, res) => {
+            const players = req.body;
 
-        app.use(express.json());
+            if (!players || !Array.isArray(players)) {
+                res.status(400).json({
+                    error: 'Array de players não fornecidos.'
+                })
+                return
+            }
 
-        app.get('/api/players', (req, res) => {
-            db.Player.findAll().then(players => {
-                res.json(players);
-            })
-        })
+            try {
+                await db.run('BEGIN TRANSACTION');
 
-        app.get('/api/players/:id', (req, res) => {
-            db.Player.findAll().then(player => {
-                res.json(player);
-            })
-        })
+                await db.run(`
+                    CREATE TABLE IF NOT EXISTS Team (
+                        id INTEGER PRIMARY KEY,
+                        conference TEXT,
+                        division TEXT,
+                        city TEXT,
+                        name TEXT,
+                        full_name TEXT,
+                        abbreviation TEXT
+                    )
+                `);
 
-        app.post('/api/players/:id', (req, res) => {
-            db.Player.create(req.body).then(player => {
-                res.json(player)
-            })
-        })
+        
+                await db.run(`
+                    CREATE TABLE IF NOT EXISTS Player (
+                        id INTEGER PRIMARY KEY,
+                        first_name TEXT,
+                        last_name TEXT,
+                        position TEXT,
+                        height TEXT,
+                        weight TEXT,
+                        jersey_number TEXT,
+                        college TEXT,
+                        country TEXT,
+                        draft_year INTEGER,
+                        draft_round INTEGER,
+                        draft_number INTEGER,
+                        team INTEGER,
+                        FOREIGN KEY (team) REFERENCES team(id)
+                    )
+                `)
 
-        app.delete('/api/players/:id', (req, res) => {
-            db.Player.destroy({
-                where: {
-                    id: req.params.id
+
+                const generatePlayer = db.prepare(`
+                    INSERT INTO Player (
+                        first_name, 
+                        last_name, 
+                        position, 
+                        height, 
+                        weight, 
+                        jersey_number,
+                        college, 
+                        country, 
+                        draft_year, 
+                        draft_round, 
+                        draft_number,
+                        team
+                    )
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                `)
+
+                const generateTeam = db.prepare(`
+                    INSERT INTO Team (
+                        id,
+                        conference,
+                        division,
+                        city,
+                        name,
+                        full_name,
+                        abbreviation
+                    )
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                `)
+
+                for (const player of players) {
+                    console.log('=====> player', player)
+
+                    generateTeam.run([
+                        player.team.id,
+                        player.team.conference,
+                        player.team.division,
+                        player.team.city,
+                        player.team.name,
+                        player.team.full_name,
+                        player.team.abbreviation,
+                    ])
+
+                    generatePlayer.run([
+                        player.first_name, 
+                        player.last_name, 
+                        player.position, 
+                        player.height, 
+                        player.weight, 
+                        player.jersey_number,
+                        player.college, 
+                        player.country, 
+                        player.draft_year, 
+                        player.draft_round, 
+                        player.draft_number,
+                        player.team
+                    ])
                 }
-            }).then(() => {
-                res.sendStatus(204);
-            }).catch(() => {
-                res.sendStatus(500)
-            })
+    
+                generatePlayer.finalize();
+                await db.run('COMMIT');
+        
+                res.status(201).json({ message: 'Jogadores Inseridos com sucesso!'})
+            } catch(error) {
+                res.status(500).json({ error: 'Erro ao criar grupo de jogadores'})
+            }
         })
+
 
         app.listen(port, () => {
             console.log(`App listening on port ${port}`) 
         })
+
+        app.use(function (req, res, next){            
+            next()
+        })
+
     } catch (error) {
         console.error(error);
     }
